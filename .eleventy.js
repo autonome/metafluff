4af1abcf64dd79ec47d550af0ad3812f2ed24426
dietrich ayala <me@burrito.space>
2023-04-27 15:57:17 +0200

    trying to fix those images

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
+const pageAssetsPlugin = require('eleventy-plugin-page-assets');
 
 module.exports = function(eleventyConfig) {
   // Add plugins
@@ -63,6 +64,12 @@ module.exports = function(eleventyConfig) {
   // Copy the `img` and `css` folders to the output
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
+
+  //eleventyConfig.addPlugin(pageAssetsPlugin, {
+  //  mode: "directory",
+  //  postsMatching: "posts/*/*.md",
+  //  hashAssets: false
+  //});
 
   // Customize Markdown library and settings:
   let markdownLibrary = markdownIt({
